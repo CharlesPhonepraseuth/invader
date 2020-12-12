5c86c279a45a8177422051b4fb4007f33c91b38e
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-12-12 11:34:12 +0100

    wip : ajout bouton valider

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ var app = {
     // we declare property wo will help us to update the board
     app.gridSizeInput = app.createInput('Taille de la grille');
     app.pixelSizeInput = app.createInput('Taille des pixels');
+    // we create buttons who will make the app more interactive
+    app.createButtons(['valider']);
   },
 
   /**
@@ -26,6 +28,20 @@ var app = {
     app.formElement.appendChild(inputElement);
 
     return inputElement;
+  },
+
+  /**
+   * create buttons from array of text
+   * @param {array} values contain text to insert 
+   */
+  createButtons(values) {
+    values.forEach(element => {
+      var buttonElement = document.createElement('button');
+      buttonElement.className = 'button';
+      buttonElement.type = 'submit';
+      buttonElement.textContent = element;
+      app.formElement.appendChild(buttonElement);
+    });
   }
 
 };
